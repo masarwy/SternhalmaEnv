4e22ec2dd4f32471607d1c93d7dac5cf9f58794e
mohammad-DR <mohammad@dailyrobotics.com>
2026-02-21 14:42:00 +0200

    fixed Discrete(N) action indexing action mask in observation PettingZoo ParallelEnv bridge for RLlib training.

---
+++ sternhalma/env/__init__.py
@@ -0,0 +1,4 @@
+from sternhalma.env.sternhalma import env, raw_env
+from sternhalma.env.rllib_wrapper import DiscreteActionMaskWrapper
+
+__all__ = ["env", "raw_env", "DiscreteActionMaskWrapper"]
--- sternhalma_v0.py
+++ sternhalma_v0.py
@@ -1,3 +1,9 @@
 from sternhalma.env.sternhalma import env, raw_env
+from sternhalma.env.rllib_wrapper import DiscreteActionMaskWrapper
 
-__all__ = ["env", "raw_env"]
+
+def rllib_env(max_actions: int = 256, **kwargs):
+    return DiscreteActionMaskWrapper(env(**kwargs), max_actions=max_actions)
+
+
+__all__ = ["env", "raw_env", "rllib_env", "DiscreteActionMaskWrapper"]
